a4078d12d50e6a4ae7292670e5da293361a1ae38
imr666 <118901326+imr666@users.noreply.github.com>
2022-11-24 11:52:44 +0300

    версия преподавателя

--- fixture_test.py
+++ fixture_test.py
@@ -2,6 +2,7 @@
 #это необходимо для тест-дизайна и правильной структуры кода.
 
 import pytest
+import random
 
 @pytest.fixture()
 def open_browser():
@@ -13,12 +14,18 @@ def open_browser():
 
 @pytest.fixture()
 def create_user(open_browser):
-    return 35
+    return 'username','password'
 
 def test_body(open_browser,create_user):
     assert open_browser == 'browser'
-    assert create_user == 5
+    assert create_user == 25
     # перейти на страницу логина
     # ввести логин и пароль
     # нажать ОК
     # убедиться, что мы перешли на страницу профиля
+
+def test_body2(open_browser, create_user):
+    pass
+
+def test_body3(open_browser,create_user):
+    pass
